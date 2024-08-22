4c3ae38ae71233cdbed0e88eb4a3be59640b0e47
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-08-22 18:18:27 -0300

    Update Program.cs
    Adição dos testes das classes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "Modelo 1", imei: "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Angry Birds Friends");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "67890", modelo: "Modelo 2", imei: "2222222222", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Angry Birds 2");
